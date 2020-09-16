6737ec98d092b83e9a6a481ef4dd018b5f8a2273
Cao <holly_zhang@apple.com>
2020-09-16 16:44:50 +0800

    fix: 判断登录情况下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,11 +5,31 @@
 
 const router = require('koa-router')()
 router.prefix('/')
+/**
+ * @description: 获取登录信息
+ * @param {type} ctx
+ * @return {type} 
+ */
+function getLoginInfo(ctx){
+    let data = {
+        isLogin:false //默认未登录
+    }
+
+    const userInfo = ctx.session.userInfo
+    if(userInfo){
+        data={
+            isLogin:true,
+            userName:userInfo.userName
+        }
+    }
+    return data
+}
+
 router.get('/login',async(ctx,next)=>{
-    await ctx.render('login',{})
+    await ctx.render('login',getLoginInfo(ctx))
 })
 
 router.get('/register',async(ctx,next)=>{
-    await ctx.render('register',{})
+    await ctx.render('register',getLoginInfo(ctx))
 })
 module.exports = router
